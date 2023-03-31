f74b9b9cfaa993b28c420c62999d98cabd130449
orbitmiyuki <114576590+orbitmiyuki@users.noreply.github.com>
2023-03-31 13:01:41 +0200

    Add files via upload

---
+++ PropertyOpdracht1/PropertyOpdracht1/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Dynamic;
+
+namespace PropertyOpdracht1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoon persoon = new Persoon();
+
+            string Naam = "gio";
+            int Leeftijd = 18;
+
+            Console.WriteLine(Naam + Leeftijd);
+
+            
+
+
+
+        }
+
+
+    }
+}
---
+++ PropertyOpdracht3/PropertyOpdracht3/Mens.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace PropertyOpdracht3
+{
+    internal class Mens
+    {
+        private int leeftijd;
+
+        public int Leeftijd
+        {
+            get
+            {
+                return leeftijd;
+            }
+            set
+            {
+                if (value > leeftijd)
+                {
+                    leeftijd = value;
+                    Console.WriteLine("im 19");
+                }
+                else
+                {
+                    Console.WriteLine("and i never learned how to read");
+                }
+            }
+        }
+
+        public Mens(int leeftijd)
+        {
+            this.leeftijd = leeftijd;
+        }
+    }
+}
