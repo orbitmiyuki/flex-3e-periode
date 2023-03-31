ae0b9a1986ee09c9505364361023897ee5487485
orbitmiyuki <114576590+orbitmiyuki@users.noreply.github.com>
2023-03-31 12:16:18 +0200

    Add files via upload

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rectangle;
+
+            rectangle = new Rectangle();
+
+            rectangle.x = 100;
+            rectangle.y = 200;
+            rectangle.width = 50;
+            rectangle.height = 50;
+
+        }
+
+    }
+
+}
---
+++ Classes/Classes/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Rectangle
+    {
+        internal int x;
+        internal int y;
+        internal int width;
+        internal int height;
+    }
+}
