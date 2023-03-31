4d17154177cc327a985a461d429278bfa06595db
orbitmiyuki <114576590+orbitmiyuki@users.noreply.github.com>
2023-03-31 12:44:54 +0200

    Add files via upload

---
+++ Time/Time/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+public static class Program
+{
+
+    public static void Main()
+    {
+        Timer t = new Timer(TimerCallback, null, 0, 1000);
+        Console.ReadLine();
+    }
+
+    private static void TimerCallback(Object o)
+    {
+        Console.Clear();
+        Console.WriteLine("In TimerCallback: " + DateTime.Now);
+    }
+}
